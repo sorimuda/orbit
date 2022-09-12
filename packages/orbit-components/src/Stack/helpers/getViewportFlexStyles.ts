747137985545e2a4993a77c39e5344e62defcd18
mainframev <vgenaev@gmail.com>
2022-09-12 22:11:14 +0200

    fix(Stack): fix getViewportFlexStyles types

--- packages/orbit-components/src/Stack/helpers/getViewportFlexStyles.ts
+++ packages/orbit-components/src/Stack/helpers/getViewportFlexStyles.ts
@@ -1,4 +1,4 @@
-import { css } from "styled-components";
+import { css, FlattenInterpolation, ThemedStyledProps } from "styled-components";
 
 import {
   getJustify,
@@ -20,25 +20,27 @@ interface StyledProps extends Props {
   theme: Theme;
 }
 
-const getViewportFlexStyles = (viewport: Devices) => (props: StyledProps) => {
+const getViewportFlexStyles = (viewport: Devices) => (
+  props: StyledProps,
+): FlattenInterpolation<ThemedStyledProps<Theme, any>> => {
   const { flex, theme } = props;
   const { inline, direction, wrap, grow, shrink, basis, justify, align, spaceAfter } = props[
     viewport
   ];
 
-  const flexStyles = flex
-    ? css`
-        display: ${getDisplay(inline)};
-        flex-direction: ${getDirection(direction)};
-        flex-wrap: ${getWrap(wrap)};
-        flex-grow: ${getGrow(grow)};
-        flex-shrink: ${getShrink(shrink)};
-        flex-basis: ${getBasis(basis)({ theme })};
-        justify-content: ${getJustify(justify)};
-        align-content: ${getAlign(align)};
-        align-items: ${getAlign(align)};
-      `
-    : "";
+  const flexStyles =
+    flex &&
+    css`
+      display: ${getDisplay(inline)};
+      flex-direction: ${getDirection(direction)};
+      flex-wrap: ${getWrap(wrap)};
+      flex-grow: ${getGrow(grow)};
+      flex-shrink: ${getShrink(shrink)};
+      flex-basis: ${getBasis(basis)({ theme })};
+      justify-content: ${getJustify(justify)};
+      align-content: ${getAlign(align)};
+      align-items: ${getAlign(align)};
+    `;
 
   return css`
     ${flexStyles};
